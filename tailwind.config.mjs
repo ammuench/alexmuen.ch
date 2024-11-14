3c8c036e9537e15b6ae7cad951702becf07d3e30
Alex Muench <ammuench@gmail.com>
2024-11-13 18:24:07 -0600

    💄 Update fonts, meta images

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,6 +5,28 @@ import daisyui from "daisyui";
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    fontFamily: {
+      heading: [
+        "Poppins",
+        "ui-sans-serif",
+        "system-ui",
+        "sans-serif",
+        "Apple Color Emoji",
+        "Segoe UI Emoji",
+        "Segoe UI Symbol",
+        "Noto Color Emoji",
+      ],
+      sans: [
+        "Roboto",
+        "ui-sans-serif",
+        "system-ui",
+        "sans-serif",
+        "Apple Color Emoji",
+        "Segoe UI Emoji",
+        "Segoe UI Symbol",
+        "Noto Color Emoji",
+      ],
+    },
     extend: {},
   },
   plugins: [tailwindTypography, daisyui],

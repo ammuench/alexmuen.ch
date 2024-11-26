0aa630700f9cd34d03498c1646c03da951cb849c
Alex Muench <ammuench@gmail.com>
2024-11-25 18:32:39 -0600

    🔍️ Add SEO, robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,35 @@
 // @ts-check
-// @ts-check
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+import robots from "astro-robots";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: "https://alexmuen.ch",
+  integrations: [
+    tailwind(),
+    sitemap(),
+    robots({
+      host: "alexmuen.ch",
+      policy: [
+        {
+          // @ts-ignore
+          userAgent: [
+            "Applebot",
+            "Googlebot",
+            "bingbot",
+            "Yandex",
+            "Yeti",
+            "Baiduspider",
+            "360Spider",
+            "Kagibot",
+            "*",
+          ],
+          allow: ["/"],
+          crawlDelay: 5,
+        },
+      ],
+    }),
+  ],
 });
